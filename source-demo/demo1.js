f9a61f0bddddb2a179187aded588879f74caab3f
ductrancs10 <tranlevietduc@gmail.com>
2018-04-08 11:58:43 +0700

    demo api

---
+++ source-demo/demo1.js
@@ -0,0 +1,19 @@
+console.log('Hello Word');
+
+var foo;
+
+foo = ['a', 3];
+
+console.log(foo[0], typeof foo);
+
+var bar = 3;
+
+
+var info = { id: 'abcde', name: 'Duc', age: 26 };
+
+console.log(info.name, info['name']);
+
+function myData() {
+    return 123; 
+} 
+console.log(myData()); 
