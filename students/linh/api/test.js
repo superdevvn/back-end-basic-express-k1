ea35259c92b3d2f050115165edba5d306c8b97e9
DESKTOP-G72575G\LINH DEP TRAI <nnlinh97it@gmail.com>
2018-04-13 08:53:32 +0700

    test

--- students/linh/api/test.js
+++ students/linh/api/test.js
@@ -17,21 +17,5 @@ list.push(a);
 list.push(b);
 list.push(1);
 list.push('linh');
-// var temp = list;
-// for (var i = 0; i < list.length - 1; i++) {
-//     for (var j = i + 1; j < list.length; j++) {
-//         if (list[i].birthdate < list[j].birthdate) {
-//             var temp = list[i];
-//             list[i] = list[j];
-//             list[j] = temp;
-//         }
-//     }
-// }
-// var len = list.length - 1;
-// li = list.reverse();
-// while(len !== 0){
-//     li.push(list[len-1]);
-//     len--;
-// }
 console.log(list);
 
